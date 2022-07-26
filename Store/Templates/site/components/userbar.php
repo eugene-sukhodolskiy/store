09ba355f375f5993cd5434fad1805c589b740cb1
Eugene <goldmikki@gmail.com>
2022-07-26 14:02:55 +0300

    User bar
    From issue `Create main navbar` https://github.com/eugene-sukhodolskiy/store/issues/7

--- Store/Templates/site/components/userbar.php
+++ Store/Templates/site/components/userbar.php
@@ -1,11 +1,11 @@
 <div class="component userbar">
 	<div class="userpic-wrapper">
-		<a href="#">
+		<a href="#" class="userpic-link">
 			<img src="https://placehold.jp/150x150.png" alt="" class="userpic">
 		</a>
 	</div>
 	<div class="user-name">
-		<a href="#">John Smith Junior</a>
+		<a href="#" class="user-name-link">John Smith Junior</a>
 	</div>
 	<div class="user-nav-wrapper">
 		<?= $this -> join("site/components/user-nav") ?>
