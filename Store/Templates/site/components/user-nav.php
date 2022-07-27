81043995510d5c9fc2753e579e8d8cc45ca9caf1
Eugene <goldmikki@gmail.com>
2022-07-27 08:29:49 +0300

    User nav
    Task from issue `Create main navbar` https://github.com/eugene-sukhodolskiy/store/issues/7

---
+++ Store/Templates/site/components/user-nav.php
@@ -0,0 +1,16 @@
+<nav class="component user-nav">
+	<ul class="user-nav-list">
+		<li class="user-nav-item">
+			<a href="#" class="user-nav-link">Профиль</a>
+		</li>
+		<li class="user-nav-item">
+			<a href="#" class="user-nav-link">Тест 1</a>
+		</li>
+		<li class="user-nav-item">
+			<a href="#" class="user-nav-link">Двинное слово</a>
+		</li>
+		<li class="user-nav-item">
+			<a href="#" class="user-nav-link">Выйти</a>
+		</li>
+	</ul>
+</nav>
