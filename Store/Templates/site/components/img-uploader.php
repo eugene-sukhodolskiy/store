fb38a249c708c13cff2ce262f12c93caea868d56
Eugene <goldmikki@gmail.com>
2022-08-05 18:50:16 +0300

    Create upload imgs component
    Issue https://github.com/eugene-sukhodolskiy/store/issues/13

--- Store/Templates/site/components/img-uploader.php
+++ Store/Templates/site/components/img-uploader.php
@@ -1,3 +1,8 @@
+<?
+	/**
+	 * @var $number_images
+	 */
+?>
 <div class="component img-uploader">
 	<div class="img-uploader-container">
 		<input 
@@ -19,7 +24,7 @@
 				Добавить фото
 			</div>
 
-			<? for($i = 0; $i < 8; $i++): ?>
+			<? for($i = 0; $i < $number_images; $i++): ?>
 				<div class="selected-img empty">
 					<span class="mdi mdi-camera"></span>
 					<div class="btns-group">
@@ -43,6 +48,6 @@
 
 <script>
 	document.addEventListener("DOMContentLoaded", e => {
-		new ImgUploader(".img-uploader-container", 8, "<?= app() -> routes -> urlto("ImgUploaderController@upload_img") ?>");
+		new ImgUploader(".img-uploader-container", <?= $number_images ?>, "<?= app() -> routes -> urlto("ImgUploaderController@upload_img") ?>");
 	});
 </script>
