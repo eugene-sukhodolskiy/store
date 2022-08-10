40e627b6dd477638ba98ee4cd6e40c656a7e34b2
Eugene <goldmikki@gmail.com>
2022-08-10 05:00:46 +0300

    Submit to draft
    From issue `Create new User Ad Post`
    https://github.com/eugene-sukhodolskiy/store/issues/11

--- Store/Controllers/UAdPostController.php
+++ Store/Controllers/UAdPostController.php
@@ -64,8 +64,8 @@ class UAdPostController extends \Store\Middleware\Controller {
 		}
 
 		if(
-			!isset($lat) or $lat == ""
-			or !isset($lng) or $lng == ""
+			!isset($lat) or $lat == "" or is_string($lat)
+			or !isset($lng) or $lng == "" or is_string($lng)
 		) {
 			return $this -> utils() -> response_error("location_not_specified");
 		}
@@ -128,11 +128,68 @@ class UAdPostController extends \Store\Middleware\Controller {
 			}
 		}
 		
-		return $this -> utils() -> response_success($_POST);
+		return $this -> utils() -> response_success();
 	}
 
 	public function create_draft() {
-		return $this -> utils() -> response_success($_POST);
+		extract($this -> get_post_data());
+
+		$title = isset($title) ? $title : "";
+		$content = isset($content) ? $content : "";
+		$condition = $condition == "new" ? 1 : $condition;
+		$condition = $condition == "used" ? 2 : $condition;
+		$country_en = !isset($country_en) ? "" : $country_en;
+		$country_ru = !isset($country_ru) ? "" : $country_ru;
+		$region_en = !isset($region_en) ? "" : $region_en;
+		$region_ru = !isset($region_ru) ? "" : $region_ru;
+		$city_en = !isset($city_en) ? "" : $city_en;
+		$city_ru = !isset($city_ru) ? "" : $city_ru;
+		$lat = isset($lat) ? floatval($lat) : 0;
+		$lng = isset($lng) ? floatval($lng) : 0;
+		$images_number = (!isset($imgs) or $imgs == "") ? 0 : substr_count($imgs, ",") + 1;
+
+		if(!isset($price) or $price == "") {
+			return $this -> utils() -> response_error("price_not_specified", [ "price" ]);
+		}
+
+		if(!isset($currency) or !in_array($currency, ["UAH", "USD", "EUR"])) {
+			$currency = "UAH";
+		}
+
+		if(!isset($exchange_flag)) {
+			$exchange_flag = 0;
+		}
+
+		if($exchange_flag == "on") {
+			$exchange_flag = 1;
+		}
+
+		$uadpost = app() -> factory -> creator() -> create_uadpost(
+			app() -> sessions -> auth_user() -> id(),
+			$title, $content, $condition, $exchange_flag, 
+			$price, $currency, $lat, $lng, $country_en, 
+			$country_ru, $region_en, $region_ru, $city_en, 
+			$city_ru, $images_number, "draft"
+		);
+
+		if(!$uadpost) {
+			return $this -> utils() -> response_error("fail_creating_uadpost");
+		}
+
+		if($images_number) {
+			$imgs_aliases = explode(",", $imgs);
+			foreach($imgs_aliases as $i => $alias) {
+				app() -> factory -> creator() -> create_image(
+					app() -> sessions -> auth_user() -> id(),
+					$uadpost -> id(),
+					"UAdPost",
+					$alias,
+					$i
+				);
+			}
+		}
+
+		return $this -> utils() -> response_success();
 	}
 
 	protected function get_post_data() {
